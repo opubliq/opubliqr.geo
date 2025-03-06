bb7510029372ac394e15ade26c677ad4850a622a
hubcad25 <hubertcadieux@gmail.com>
2025-03-05 20:29:48 -0500

    ajout des districts shapefiles de quebec au package

---
+++ R/spatial_districts_quebec.R
@@ -0,0 +1,8 @@
+#' Données géospatiales de la ville de Québec – Districts électoraux municipaux (2012)
+#'
+#' Objet sf contenant les districts électoraux de la ville de Québec, provenant de [Données Québec](https://www.donneesquebec.ca/recherche/dataset/ca1847da-e908-43de-bd8c-588570331650).
+#'
+#' @format Un objet `sf` avec une colonne `geometry` représentant les polygones des circonscriptions.
+#' @source Statistique Canada
+#' @references VILLE DE QUÉBEC. Districts électoraux, [Jeu de données], dans Données Québec, 2012, mis à jour le 24 février 2025. [https://www.donneesquebec.ca/recherche/dataset/vque_43], (consulté le 05 mars 2025).
+"spatial_districts_quebec"
--- data-raw/vision_opubliq/quebec_2025/quebec/02_load_shapefiles_as_rda_to_package.R
+++ data-raw/vision_opubliq/quebec_2025/quebec/02_load_shapefiles_as_rda_to_package.R
@@ -1,3 +1,5 @@
+# Ce script load les shapefiles des districts électoraux de la Ville de Qc dans le package 
+
 spatial_districts_quebec <- sf::read_sf("data-raw/data/vision_opubliq/quebec_2025/quebec/vdq-districtelectoral.shp")
 
 save(spatial_districts_quebec, file = "data/spatial_districts_quebec.rda", compress = "bzip2")
