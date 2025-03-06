66abf3531d3d92ce663e839fe5878e2bfa03e6a1
hubcad25 <hubertcadieux@gmail.com>
2025-03-05 20:50:06 -0500

    remove third dimension in districts shapefiles

--- data-raw/vision_opubliq/quebec_2025/quebec/02_load_shapefiles_as_rda_to_package.R
+++ data-raw/vision_opubliq/quebec_2025/quebec/02_load_shapefiles_as_rda_to_package.R
@@ -1,5 +1,8 @@
 # Ce script load les shapefiles des districts électoraux de la Ville de Qc dans le package 
 
-spatial_districts_quebec <- sf::read_sf("data-raw/data/vision_opubliq/quebec_2025/quebec/vdq-districtelectoral.shp")
+spatial_districts_quebec <- sf::read_sf("data-raw/data/vision_opubliq/quebec_2025/quebec/vdq-districtelectoral.shp") |>
+  dplyr::mutate(
+    geometry = sf::st_zm(geometry)
+  )
 
 save(spatial_districts_quebec, file = "data/spatial_districts_quebec.rda", compress = "bzip2")
